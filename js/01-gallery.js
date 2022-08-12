2697ceb3f161b4b0735e941dea3e413fb8cc190a
Jonny-Sky <99253125+Jonny-Sky@users.noreply.github.com>
2022-08-12 17:21:27 +0300

    заканчивает первое задание

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,21 +1,38 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-// const galleryList = ()
 
-const renderList = (galleryItems) => {
-  const result = galleryItems.map(
-    (item) => `<div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
-    <img
-      class="gallery__image"
-      src="small-image.jpg"
-      data-source="large-image.jpg"
-      alt="Image description"
-    />
-  </a>
-</div>`
-  );
-  return result;
-};
-console.log(renderList(galleryItems));
+const containerEl = document.querySelector(".gallery");
+const renderList = (galleryItems) =>
+  galleryItems
+    .map(
+      ({ preview, original, description }) => `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`
+    )
+    .join("");
+
+containerEl.insertAdjacentHTML("beforeend", renderList(galleryItems));
+containerEl.addEventListener("click", selectPicture);
+
+function selectPicture(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const swatchEl = event.target.dataset.source;
+  const instance = basicLightbox.create(`<img src=${swatchEl}>`);
+
+  // console.log(swatchEl);
+  instance.show();
+}
+
+// console.log(swatchEl);
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const containerEl = document.querySelector(".gallery");
+
+function markupRender(galleryItems) {
+  return galleryItems
+    .map(({ description, original, preview }) => {
+      return `
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>`;
+    })
+    .join("");
+}
+containerEl.insertAdjacentHTML("beforeend", markupRender(galleryItems));
+// containerEl.addEventListener("click", selectPicture);
+new SimpleLightbox(".some-element a", {
+  /* options */
+});
 console.log(galleryItems);
+console.log(markupRender);
